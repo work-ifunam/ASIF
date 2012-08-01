f45af1d20de4077589609042c2dff46ba9eb24b3
Gustavo GM <gozunam@gmail.com>
2012-08-01 18:20:13 -0500

    Adding more authorization rules and removing edit/update actions from assignations

--- app/controllers/assignations_controller.rb
+++ app/controllers/assignations_controller.rb
@@ -1,9 +1,8 @@
 class AssignationsController < ApplicationController
-
+  
   before_filter :require_user
-
-  # GET /assignations
-  # GET /assignations.json
+  load_and_authorize_resource
+  
   def index
     @assignation_user = Assignation.user(current_user.category);
     respond_to do |format|
@@ -11,19 +10,7 @@ class AssignationsController < ApplicationController
       format.json { render json: @assignations }
     end
   end
-
-  # GET /assignations/1
-  # GET /assignations/1.json
-  def show
-    @assignation = Assignation.find(params[:id])
-      respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @assignation }
-    end
-  end
-
-  # GET /assignations/new
-  # GET /assignations/new.json
+  
   def new
     @assignation = Assignation.new
     @category = Category.assignation(current_user.category);
@@ -33,16 +20,7 @@ class AssignationsController < ApplicationController
       format.json { render json: @assignation }
     end
   end
-
-  # GET /assignations/1/edit
-  def edit
-    @assignation = Assignation.find(params[:id])
-    @category = Category.assignation(current_user.category);
-    @user = User.assignation(current_user.category);
-  end
-
-  # POST /assignations
-  # POST /assignations.json
+  
   def create
     @assignation = Assignation.new(params[:assignation])
     respond_to do |format|
@@ -55,30 +33,11 @@ class AssignationsController < ApplicationController
       end
     end
   end
-
-  # PUT /assignations/1
-  # PUT /assignations/1.json
-  def update
-    @assignation = Assignation.find(params[:id])
-    @category = Category.assignation(current_user.category);
-    @user = User.assignation(current_user.category);
-    respond_to do |format|
-      if @assignation.update_attributes(params[:assignation])
-        format.html { redirect_to action: "index" }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @assignation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /assignations/1
-  # DELETE /assignations/1.json
+  
   def destroy
     @assignation = Assignation.find(params[:id])
-#    @category = Category.assignation(current_user.category);
-#    @user = User.assignation(current_user.category);
+   # @category = Category.assignation(current_user.category);
+   # @user = User.assignation(current_user.category);
     @assignation.destroy
     respond_to do |format|
       format.html { redirect_to assignations_url }
