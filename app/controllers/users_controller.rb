abb6df4f86a14a5504399ddcf567a4c3c36adc78
Francisco Amaya <francisco.garduno.amaya@gmail.com>
2012-08-02 13:26:23 -0500

    Tuning viewsx!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to action: 'index'}
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to action: 'index' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,9 +4,9 @@ class Ticket < ActiveRecord::Base
 
     #Preguntaer si los tickets que se veran son los creados por el mismo usuario o quienes ven estos tickets
     def self.user(department)
-       if department.eql?("Administrador computo")  || department.eql?("Tecnico computo")
+       if department.eql?("Admin COMPUTO")  || department.eql?("Personal COMPUTO")
           return Ticket.all
-       elsif department.eql?("Administrador electronica") || department.eql?("Tecnico electronica") 
+       elsif department.eql?("Admin ELECTRONICA") || department.eql?("Personal ELECTRONICA") 
           return Ticket.all
        else
           return Category.find(:all,:conditions => 'department = "null"')
