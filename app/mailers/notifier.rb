68b9e0949f438c516ac08fd042edbb9da20d665e
DannasCornell <dannas.cornell@gmail.com>
2022-06-10 23:33:44 -0500

    Secretary added to computer mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -126,7 +126,7 @@ def send_reply_to_user(ticket)
     end
     @technicians = @techs.join(",")
     if ticket.department == "computo"
-      @email = ticket.user.email + "," + " " + "maricelabarrera@fisica.unam.mx"
+      @email = ticket.user.email + "," + " " + "maricelabarrera@fisica.unam.mx, angelica@fisica.unam.mx"
       #@email = ticket.user.email 
     elsif ticket.department == "comunicacion"
       @email = ticket.user.email
