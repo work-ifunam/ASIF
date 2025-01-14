cda25266031aba6bc1f1a4bcc4a91c1b17e8d665
DannasCornell <dannas.cornell@gmail.com>
2025-01-14 13:42:20 -0600

    Adding Dirección role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,20 +12,16 @@ class Ability
       can :manage, Assignment          
       can :manage, Score
       can :manage, Ticket
-    elsif user.category == "Admin COMPUTO" || user.category == "SAC" || user.category == "Sec COMPUTO"
+    elsif user.category == "Admin COMPUTO" || user.category == "SAC" || user.category == "Sec COMPUTO" || user.category == "Dirección"
       can :read, :show_computer_tickets
       #cannot :read, :show_workshop_tickets
-    elsif user.category == "Admin TALLER"  || user.category == "SAC" || user.category == "Sec COMPUTO"
+    elsif user.category == "Admin TALLER"  || user.category == "SAC" || user.category == "Sec COMPUTO" || user.category == "Dirección"
       can :read, :show_workshop_tickets
-    elsif user.category == "Admin MANTENIMIENTO"  || user.category == "SAC" || user.category == "Sec COMPUTO"
+    elsif user.category == "Admin MANTENIMIENTO"  || user.category == "SAC" || user.category == "Sec COMPUTO" || user.category == "Dirección"
       can :read, :show_maintenance_tickets
-    elsif user.category == "Admin COMUNICACION"  || user.category == "SAC" || user.category == "Sec COMPUTO"
+    elsif user.category == "Admin COMUNICACION"  || user.category == "SAC" || user.category == "Sec COMPUTO" || user.category == "Dirección"
       can :read, :show_communication_tickets
     elsif user.category == "Admin COMPUTO" || user.category == "Admin ELECTRONICA" || user.category == "Admin TALLER"  || user.category == "Admin COMUNICACION" || user.category == "Admin MANTENIMIENTO" || user.category == "Admin SERVICIOS" || user.category == "SAC" || user.category == "Sec COMPUTO"
-      can :manage, Category
-      can :manage, Assignation          
-      can :manage, Assignment          
-    elsif user.category == "Personal COMPUTO" || user.category == "Personal ELECTRONICA"
       can :manage, Category
     else
       can :read, :all
